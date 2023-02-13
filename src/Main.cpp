713360cd98711f364c1a892207d6f2f89dd077b8
daffa <daffa.fathan9@gmail.com>
2023-02-14 02:48:10 +0700

    perbaikan

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-int main(){
-
+int main()
+{
 
 	int i;
 	string pass, user;
@@ -17,117 +17,134 @@ int main(){
 	i = 1;
 	char re;
 
-
-
-do{
+	do
+	{
 
 		system("cls");
 		system("color a");
-		cout<<"\n\n"<<endl;
-		cout<<"\t==============================================="<<endl;
-		cout<<"\t  Selamat Datang Di Form Login Fanthom Store   "<<endl;
-		cout<<"\t==============================================="<<endl;
-		cout<<"\t                                               "<<endl;
-		cout<<"\t           Username : ";cin>>user;
-		cout<<"\t                                               "<<endl;
-		cout<<"\t           Password : ";cin>>pass;
-		cout<<"\t                                               "<<endl;
-		cout<<"\t                                               "<<endl;
-		cout<<"\t==============================================="<<endl;
+		cout << "\n\n"
+				 << endl;
+		cout << "\t===============================================" << endl;
+		cout << "\t  Selamat Datang Di Form Login Fanthom Store   " << endl;
+		cout << "\t===============================================" << endl;
+		cout << "\t                                               " << endl;
+		cout << "\t           Username : ";
+		cin >> user;
+		cout << "\t                                               " << endl;
+		cout << "\t           Password : ";
+		cin >> pass;
+		cout << "\t                                               " << endl;
+		cout << "\t                                               " << endl;
+		cout << "\t===============================================" << endl;
 
-		
-		if(user == username && pass == password){
-			cout<<"\n\n";
-			cout<<"\t           Selamat datang tuan"<<endl;
-			cout<<"\t           Login Anda Berhasil"<<endl;
+		if (user == username && pass == password)
+		{
+			cout << "\n\n";
+			cout << "\t           Selamat datang tuan" << endl;
+			cout << "\t           Login Anda Berhasil" << endl;
 			login = "berhasil";
 			break;
 		}
 
-		else if(user != username && pass != password){
-			cout<<"\tMaaf, ID & Password Salah!"<<endl;
+		else if (user != username && pass != password)
+		{
+			cout << "\tMaaf, ID & Password Salah!" << endl;
 			i++;
-			lagi:
-			cout<<"\tCoba lagi? [y/n] : ";cin>>re;
-				if(re=='Y'||re=='y'){
-					continue;
-				}
-				else if(re=='N'||re=='n'){
-					goto end;				
-				}
-				else{
-					goto lagi;
-				}
-				
-	}
+		lagi:
+			cout << "\tCoba lagi? [y/n] : ";
+			cin >> re;
+			if (re == 'Y' || re == 'y')
+			{
+				continue;
+			}
+			else if (re == 'N' || re == 'n')
+			{
+				goto end;
+			}
+			else
+			{
+				goto lagi;
+			}
+		}
 
-	else if(user != username){
-			cout<<"\tUsername yang anda masukan salah"<<endl;
+		else if (user != username)
+		{
+			cout << "\tUsername yang anda masukan salah" << endl;
 			i++;
-			lagi2:
-			cout<<"\tCoba lagi? [y/n] : ";cin>>re;
-				if(re=='Y'||re=='y'){
-					continue;
-				}
-				else if(re=='N'||re=='n'){
-					goto end;				
-				}
-				else{
-					goto lagi2;
-				}
-				
-	}
+		lagi2:
+			cout << "\tCoba lagi? [y/n] : ";
+			cin >> re;
+			if (re == 'Y' || re == 'y')
+			{
+				continue;
+			}
+			else if (re == 'N' || re == 'n')
+			{
+				goto end;
+			}
+			else
+			{
+				goto lagi2;
+			}
+		}
 
-	else if(pass != password){
-			cout<<"\tPassword yang anda masukan salah"<<endl;
+		else if (pass != password)
+		{
+			cout << "\tPassword yang anda masukan salah" << endl;
 			i++;
-			lagi3:
-			cout<<"\tCoba lagi? [y/n] : ";cin>>re;
-				if(re=='Y'||re=='y'){
-					continue;
-				}
-				else if(re=='N'||re=='n'){
-					goto end;				
-				}
-				else{
-					goto lagi3;
-				}
-				
-	}
-
-}
-
-	while(i<=3);
-
-	if(login != "berhasil"){
-
-		for(i=60; i>0; i--){
-		cout<<"\n"<<endl;
-		cout<<"\t============================================================"<<endl;
-		cout<<"\tAnda telah 3x memasukan ID dan Password yang salah"<<endl;
-		cout<<"\tMohon maaf akun anda kami blokir untuk sementara..."<<endl;
-		cout<<"\tSilahkan hubungi kami melalui E-Mail Daffa.fathan9@gmail.com"<<endl;
-		cout<<"\tCountdown : "<<i<<" Detik lagi";
-		Sleep(1000);
-		system("cls");
-	}
-
-	if(i==0){
-		return main();
-	}
+		lagi3:
+			cout << "\tCoba lagi? [y/n] : ";
+			cin >> re;
+			if (re == 'Y' || re == 'y')
+			{
+				continue;
+			}
+			else if (re == 'N' || re == 'n')
+			{
+				goto end;
+			}
+			else
+			{
+				goto lagi3;
+			}
+		}
 
 	}
 
+	while (i <= 3);
 
+	if (login != "berhasil")
+	{
 
-	end:
-	cout<<endl<<endl;
-	cout<<"\t============TERIMA KASIH============="<<endl;
+		for (i = 60; i > 0; i--)
+		{
+			cout << "\n"
+					 << endl;
+			cout << "\t============================================================" << endl;
+			cout << "\tAnda telah 3x memasukan ID dan Password yang salah" << endl;
+			cout << "\tMohon maaf akun anda kami blokir untuk sementara..." << endl;
+			cout << "\tSilahkan hubungi kami melalui E-Mail Daffa.fathan9@gmail.com" << endl;
+			cout << "\tCountdown : " << i << " Detik lagi";
+			Sleep(1000);
+			system("cls");
+		}
 
-	cout<<endl<<endl;
+		if (i == 0)
+		{
+			return main();
+		}
+	}
+
+end:
+	cout << endl
+			 << endl;
+	cout << "\t============TERIMA KASIH=============" << endl;
+
+	cout << endl
+			 << endl;
 	time_t waktu;
 	time(&waktu);
-	cout<<"\t"<<ctime(&waktu)<<endl;
+	cout << "\t" << ctime(&waktu) << endl;
 	cin.get();
 	return 0;
 }
